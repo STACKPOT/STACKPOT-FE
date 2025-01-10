78bd0472506eec2c3ea2e793620fa41804d7ebbe
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-11 04:14:33 +0900

    feat: 상태 뱃지 UI 구현

---
+++ src/components/commons/Badge/StateBadge/StateBadge.style.ts
@@ -0,0 +1,12 @@
+import { css } from "@emotion/react";
+import theme from "@styles/theme";
+
+export const badgeStyle = (color: string) => css`
+    display: inline-flex;
+    padding: 0.8rem 1.2rem;
+    background-color: ${color};
+    color: white;
+    border: none;
+    border-radius: 3.2rem;
+    ${theme.font.captionBold1}
+`
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Button from "@components/commons/Button/Button";
 import Header from "@components/layouts/Header/Header";
 import TextField from "@components/commons/TextField/TextField";
 import PotCard from "./cards/PotCard/PotCard";
+import StateBadge from "./commons/Badge/StateBadge/StateBadge";
 
-export { Button, Header, TextField, PostCard, PotCard };
+export { Button, Header, TextField, PostCard, PotCard, StateBadge };
