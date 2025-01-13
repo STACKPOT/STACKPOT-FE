a55efd4081acef29a3f40a94529d6f2f4eab49c2
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-14 01:10:33 +0900

    feat: 멤버 평가 뱃지 UI 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,5 +10,6 @@ import TaskCard from "./cards/TaskCard/TaskCard";
 import MemberGroup from "./commons/Badge/MemberGroup/MemberGroup";
 import CommonBadge from "./commons/Badge/CommonBadge/CommonBadge";
 import NumberBadge from "./commons/Badge/NumberBadge/NumberBadge";
+import EvaluationBadge from "./commons/Badge/EvaluationBadge/EvaluationBagde";
 
-export { Button, Header, TextField, PostCard, PotCard, StateBadge, DdayBadge, CategoryButton, TaskCard, MemberGroup, CommonBadge, NumberBadge };
+export { Button, Header, TextField, PostCard, PotCard, StateBadge, DdayBadge, CategoryButton, TaskCard, MemberGroup, CommonBadge, NumberBadge, EvaluationBadge };
