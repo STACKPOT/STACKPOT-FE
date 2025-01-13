6c67fe745772dd20c0f21d3356656d4bfcd6df90
숨 <stacycho0728@naver.com>
2025-01-13 21:18:35 +0900

    style : footer 코드 피드백 반영 수정

--- src/components/layouts/Footer/Footer.style.ts
+++ src/components/layouts/Footer/Footer.style.ts
@@ -2,10 +2,8 @@ import { css } from "@emotion/react";
 import theme from "@styles/theme";
 
 export const footerStyle = css`
-  width: 100%;
-  padding: 60px 140px;
+  padding: 6rem 14rem;
   background: ${theme.color.point.ivory};
-  border-top: 4px solid ${theme.color.point.hero};
   display: flex;
   justify-content: center;
   align-items: center;
@@ -15,41 +13,14 @@ export const contentStyle = css`
   flex: 1 1 0;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
   align-items: flex-start;
   gap: 20px;
 `;
 
 export const sectionStyle = css`
   display: flex;
-  justify-content: flex-start;
   align-items: flex-start;
   gap: 40px;
-`;
-
-export const linkStyle = css`
-  display: flex;
-  gap: 8px;
-  align-items: center;
-  font-size: 16px;
-  font-family: "Pretendard";
-  font-weight: 500;
-  line-height: 28px;
-  color: ${theme.color.interactive.inactive};
-`;
-
-export const textStyle = css`
-  font-size: 16px;
-  font-family: "Pretendard";
-  font-weight: 500;
-  line-height: 28px;
-  color: ${theme.color.interactive.inactive};
-`;
-
-export const legalStyle = css`
-  font-size: 16px;
-  font-family: "Pretendard";
-  font-weight: 500;
-  line-height: 28px;
+  ${theme.font.caption3}
   color: ${theme.color.interactive.inactive};
 `;
--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,37 +1,26 @@
-import { css } from "@emotion/react";
 import { Logo } from "@assets/svgs";
-import theme from "@styles/theme";
-import {
-  footerStyle,
-  contentStyle,
-  sectionStyle,
-  linkStyle,
-  textStyle,
-  legalStyle,
-} from "./Footer.style";
+import { footerStyle, contentStyle, sectionStyle } from "./Footer.style";
 
 const Footer: React.FC = () => {
   return (
-    <div css={footerStyle}>
+    <footer css={footerStyle}>
       <div css={contentStyle}>
         <Logo />
         <div css={sectionStyle}>
-          <div css={textStyle}>TEAM STACKPOT</div>
-          <div css={linkStyle}>
-            <div>고객센터 | 010-1234-5678</div>
-          </div>
-          <div css={linkStyle}>
-            <div>메일 | stackpot@example.com</div>
-          </div>
+          <p>TEAM STACKPOT</p>
+          <p>고객센터 | 010-1234-5678</p>
+          <p>메일 | stackpot@example.com</p>
         </div>
         <div css={sectionStyle}>
-          <div css={textStyle}>이용약관</div>
-          <div css={textStyle}>개인정보처리방침</div>
-          <div css={textStyle}>문의하기</div>
+          <p>이용약관</p>
+          <p>개인정보처리방침</p>
+          <p>문의하기</p>
         </div>
-        <div css={legalStyle}>© 2024 stackpot. All rights reserved.</div>
+        <div css={sectionStyle}>
+          <p>© 2024 stackpot. All rights reserved.</p>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
