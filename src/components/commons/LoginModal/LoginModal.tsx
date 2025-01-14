cc70316f5d3c62bc917b77e22e1647fb504ffa6f
숨 <stacycho0728@naver.com>
2025-01-14 17:29:14 +0900

    refactor: 로그인모달 수정

--- src/components/commons/LoginModal/LoginModal.tsx
+++ src/components/commons/LoginModal/LoginModal.tsx
@@ -1,35 +1,55 @@
 import { Kakao } from "@assets/svgs";
 import { dialogStyles } from "./LoginModal.style";
-import { CloseBtn } from "@assets/svgs";
+import { CloseBtn, Logo } from "@assets/svgs";
+import theme from "@styles/theme";
+import { css } from "@emotion/react";
 
 interface LoginModalProps {
-  title: string;
   context: string;
   onLogin: () => void;
   onCancel: () => void;
 }
 
-const LoginModal: React.FC<LoginModalProps> = ({
-  title,
-  onLogin,
-  onCancel,
-}) => {
+const LoginModal: React.FC<LoginModalProps> = ({ onLogin, onCancel }) => {
   return (
     <div css={dialogStyles.container}>
-      <div css={dialogStyles.header}>
-        {title}
-        <CloseBtn onClick={onCancel} />
-      </div>
-      <div css={dialogStyles.body}>
-        <Kakao css={dialogStyles.kakaoIcon} onClick={onLogin} />
-      </div>
-      <div css={dialogStyles.footer}>
-        로그인 시,{" "}
-        <a href="#" css={dialogStyles.termsLink}>
-          서비스 이용약관
-        </a>
-        에 동의하는 것으로 간주됩니다.
-      </div>
+      <header css={dialogStyles.closeIcon} onClick={onCancel}>
+        <CloseBtn />
+      </header>
+      <body css={dialogStyles.body}>
+        <Logo />
+        <div css={dialogStyles.section}>
+          <p
+            css={css`
+              color: ${theme.color.point.navy};
+              ${theme.font.title1};
+            `}
+          >
+            STACKPOT에 오신 것을 환영합니다!
+          </p>
+          <p
+            css={css`
+              color: ${theme.color.object.assistive};
+              ${theme.font.caption3};
+            `}
+          >
+            로그인하시고 프로젝트를 진행해 보세요.
+          </p>
+        </div>
+        <Kakao onClick={onLogin} />
+        <p
+          css={css`
+            color: ${theme.color.object.hero};
+            ${theme.font.caption2};
+          `}
+        >
+          로그인 시,{" "}
+          <a href="#" css={dialogStyles.termsLink}>
+            서비스 이용약관
+          </a>
+          에 동의하는 것으로 간주됩니다.
+        </p>
+      </body>
     </div>
   );
 };
