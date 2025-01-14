61b92bda6f6507e0a294902af88ac6dea2eba87c
Sohyunnnn <thgus05061@naver.com>
2025-01-15 02:29:29 +0900

    asset: add assets

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,6 @@
 export { default as Logo } from "./logo.svg?react";
+export { default as HomeIcon } from "./home.svg?react";
+export { default as MyPotIcon } from "./myPot.svg?react";
+export { default as PotIcon } from "./pot.svg?react";
+export { default as SetUpIcon } from "./setUp.svg?react";
+export { default as Profile } from "./profile.svg?react";
