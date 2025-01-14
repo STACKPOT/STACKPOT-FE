3161c0543b6d80ac1440b2d6159f79009c6eb73e
숨 <stacycho0728@naver.com>
2025-01-14 18:34:00 +0900

    style: 모달 스타일 수정

--- src/components/commons/Modal/Modal.style.ts
+++ src/components/commons/Modal/Modal.style.ts
@@ -3,57 +3,53 @@ import theme from "@styles/theme";
 
 export const modalStyles = {
   container: css`
-    width: 100%;
-    max-width: 45rem;
+    width: 50rem;
+    height: 30.4rem;
     background: ${theme.color.base.white};
-    box-shadow: 0rem 0.4rem 0.8rem rgba(0, 0, 0, 0.13);
+    box-shadow: 0rem 0rem 0.1rem rgba(0, 0, 0, 0.04);
+    border: 1px solid ${theme.color.object.alternative};
     border-radius: 2.4rem;
-    display: flex;
+    display: inline-flex;
     flex-direction: column;
   `,
   header: css`
-    width: 100%;
     padding: 1.6rem 2.4rem;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-  `,
-  title: css`
-    color: ${theme.color.base.black};
-    ${theme.font.title1};
-  `,
-  closeIcon: css`
-    width: 2.8rem;
-    height: 2.8rem;
-    display: flex;
-    justify-content: center;
+    justify-content: end;
     align-items: center;
     cursor: pointer;
   `,
   body: css`
-    width: 100%;
-    padding: 6.2rem 2.4rem;
-    text-align: center;
-    color: ${theme.color.object.assistive};
-    ${theme.font.caption3};
+    width: 50rem;
+    height: 15rem;
+    padding: 1.6rem 2.4rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1.6rem;
   `,
 
   footer: css`
-    height: 6.4rem;
-    width: 100%;
+    width: 50rem;
+    height: 9.4rem;
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 0.8rem;
-    gap: 1.6rem;
-    border-top: 0.1rem solid ${theme.color.border.normal};
+    padding: 1rem 0 1rem 0;
+    gap: 3rem;
   `,
 
-  button: css`
-    width: 20.3rem;
-    background: none;
-    border: none;
+  button: (backgroundColor: string) => css`
+    width: 19.4rem;
+    height: 5rem;
+    padding: 0.6rem 2.4rem;
+    background: ${backgroundColor};
+    color: ${theme.color.point.ivory};
+    border-radius: 1.6rem;
+    border: 0.1rem solid ${backgroundColor};
     ${theme.font.caption3};
+    text-align: center;
     cursor: pointer;
   `,
 };
