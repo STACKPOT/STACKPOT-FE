b6e19c0a53277256da27ab538d56aa4598db515e
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-15 02:10:22 +0900

    rename: svg asset 파일명 수정

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,6 +1,5 @@
 export { default as Logo } from "./logo.svg?react";
-export { default as LikeIcon } from "./likeIcon.svg?react";
-export { default as SaveIcon } from "./saveIcon.svg?react";
-export { default as MoreButton } from "./moreButton.svg?react"
-export { default as CalendarIcon } from "./calendarIcon.svg?react"
-export { default as AddMemberButton } from "./addMemberButton.svg?react"
+export { default as LikeIcon } from "./like.svg?react";
+export { default as SaveIcon } from "./save.svg?react";
+export { default as MoreButton } from "./More horizontal.svg?react"
+export { default as CalendarIcon } from "./calendar.svg?react"
