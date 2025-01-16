01c256100da3edd7a358cca8487b7ee11d79d6a8
Sohyunnnn <thgus05061@naver.com>
2025-01-17 01:34:13 +0900

    style: index 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,7 @@ import HomePage from "./Home/Home";
 import MyPage from "./MyPage/MyPage";
 import LandingPage from "./Landing/Landing";
 import NotFound from "./NotFound/NotFound";
+import SearchPage from "./Search/Search";
+import SettingPage from "./Setting/Setting";
 
-export { HomePage, MyPage, LandingPage, NotFound };
+export { HomePage, MyPage, LandingPage, NotFound, SearchPage, SettingPage };
