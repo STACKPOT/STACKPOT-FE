2f27048e7031313cbfd1efa2ca104034f2cf7674
DeoKanKoong <rla9046@gmail.com>
2025-01-17 04:51:39 +0900

    refactor: Sidebar 따라다니는 모션 추가 & 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { globalStyles } from "@styles/global";
 import theme from "@styles/theme";
 import { Outlet } from "react-router-dom";
 import { Header } from "./components";
+import SideBar from "@components/layouts/SideBar/SideBar";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
       <Header />
+      <SideBar />
       <Outlet />
     </ThemeProvider>
   );
-}
+};
 
 export default App;
