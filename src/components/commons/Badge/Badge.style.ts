49e5773aece19c77167dcab226b933cc5c648799
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-17 23:23:04 +0900

    style: 뱃지 border 색상 수정

--- src/components/commons/Badge/Badge.style.ts
+++ src/components/commons/Badge/Badge.style.ts
@@ -6,7 +6,7 @@ export const badgeStyle = css`
     height: 2.2rem;
     padding: 0.4rem 1.5rem;
     border-radius: 3.2rem;
-    border: 0.1rem solid rgba(112, 115, 124, 0.20);
+    border: 0.1rem solid ${theme.color.border.normal};
     background-color: ${theme.color.point.normal};
     color: ${theme.color.base.white};
     ${theme.font.caption1}
