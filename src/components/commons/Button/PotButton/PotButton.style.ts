1ecbb7a80cf1387cffd08fa3fcf5b4a038bc9678
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-18 14:52:44 +0900

    style: box-shadow 적용

--- src/components/commons/Button/PotButton/PotButton.style.ts
+++ src/components/commons/Button/PotButton/PotButton.style.ts
@@ -8,6 +8,7 @@ export const buttonStyle = css`
     border: 0.1rem solid ${theme.color.point.alternative};
     background-color: ${theme.color.base.white};
     color: ${theme.color.point.hero};
+    box-shadow: 0px 4px 12px 0px rgba(13, 10, 44, 0.06);
     ${theme.font.captionBold1};
     transition: all 0.3s ease;
 
