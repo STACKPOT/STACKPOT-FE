1c716c1e4c3d35e6cb82a11eb1255af95af65cfb
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-19 04:25:49 +0900

    refactor: 버튼 선택 로직 수정

--- src/components/commons/Badge/CategoryButton/CategoryButton.tsx
+++ src/components/commons/Badge/CategoryButton/CategoryButton.tsx
@@ -1,24 +1,17 @@
-import { useState } from "react";
 import { selectedButtonStyle, unselectedButtonStyle } from "./CategoryButton.style"
 
 interface CategoryButtonProps {
     children: string,
     selected: boolean,
-    onClick: () => void;
+    onClick: (category:string) => void;
 }
 
 const CategoryButton: React.FC<CategoryButtonProps> = ({ children, selected, onClick }: CategoryButtonProps) => {
-    const [select, setSelect] = useState(selected);
-
-    const handleClick = () => {
-        setSelect(!select);
-        onClick();
-    }
     return (
         <>
-            {select ?
-                <button css={selectedButtonStyle} onClick={handleClick}>{children}</button> :
-                <button css={unselectedButtonStyle} onClick={handleClick}>{children}</button>
+            {selected ?
+                <button css={selectedButtonStyle} onClick={() =>onClick(children)}>{children}</button> :
+                <button css={unselectedButtonStyle} onClick={()=>onClick(children)}>{children}</button>
             }
         </>
     )
