4a9806dc37e1996162b822ed8c589571cb70d64b
숨 <stacycho0728@naver.com>
2025-01-20 17:28:11 +0900

    refactor: labelCss, inputCss 기능 추가

--- src/components/commons/TextField/TextField.tsx
+++ src/components/commons/TextField/TextField.tsx
@@ -1,24 +1,27 @@
 import { inputStyle, labelStyle, textStyle } from "./TextField.style";
+import { css, SerializedStyles } from "@emotion/react";
 
 interface TextFieldProps {
   placeholder: string;
   label?: string;
   supportingText?: string;
+  labelCss?: SerializedStyles;
+  inputCss?: SerializedStyles;
 }
 
 const TextField: React.FC<TextFieldProps> = ({
   placeholder,
   label,
   supportingText,
+  labelCss,
+  inputCss,
 }) => {
   return (
-    <>
-      <label css={labelStyle}>
-        {label}
-        <input placeholder={placeholder} css={inputStyle} />
-        <p css={textStyle}>{supportingText}</p>
-      </label>
-    </>
+    <label css={[labelStyle, labelCss]}>
+      {label}
+      <input placeholder={placeholder} css={[inputStyle, inputCss]} />
+      {supportingText && <p css={textStyle}>{supportingText}</p>}
+    </label>
   );
 };
 
