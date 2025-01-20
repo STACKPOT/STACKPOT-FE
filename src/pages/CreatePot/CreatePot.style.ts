259dd5e5dc68f1f89df1b56c314b2b7d9f95b904
Sohyunnnn <thgus05061@naver.com>
2025-01-20 16:23:37 +0900

    feat: 나의 팟 만들기 페이지 퍼블리싱

---
+++ src/pages/CreatePot/CreatePot.style.ts
@@ -0,0 +1,83 @@
+import { css } from "@emotion/react";
+import theme from "@styles/theme";
+
+export const mainContainer = css`
+  margin-top: 4.8rem;
+`;
+
+export const headContainer = css`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const titleStyle = css`
+  ${theme.font.bodyBold2}
+  color: ${theme.color.base.darkgray};
+`;
+
+export const titleContainer = css`
+  display: flex;
+  gap: 1.6rem;
+  align-items: center;
+`;
+
+export const iconStyle = css`
+  color: ${theme.color.point.hero};
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+export const formContainer = css`
+  padding: 3.6rem 10rem;
+  border: 1px solid ${theme.color.object.alternative};
+  margin-top: 3.2rem;
+  border-radius: 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
+export const inputStyle = css`
+  margin-left: 4.4rem;
+  padding: 1.2rem 1.6rem;
+  ${theme.font.caption3}
+  display: flex;
+  flex-grow: 1;
+  border-radius: 8px;
+  border: 1px solid ${theme.color.object.alternative};
+  &::placeholder {
+    color: ${theme.color.interactive.inactive};
+  }
+`;
+
+export const labelStyle = css`
+  display: flex;
+  color: ${theme.color.base.darkgray};
+  display: flex;
+  align-items: center;
+  ${theme.font.caption3}
+`;
+
+export const dividerStyle = css`
+  height: 1px;
+  width: 100%;
+  background-color: ${theme.color.object.alternative};
+`;
+
+export const buttonContainer = css`
+  display: flex;
+  gap: 2.4rem;
+  margin-left: 3rem;
+`;
+
+export const textareaStyle = css`
+  padding: 2.4rem;
+  height: 40.6rem;
+  font-family: "Pretendard";
+  border-radius: 12px;
+  ${theme.font.caption3}
+  resize: none;
+  &::placeholder {
+    color: ${theme.color.object.hero};
+  }
+`;
