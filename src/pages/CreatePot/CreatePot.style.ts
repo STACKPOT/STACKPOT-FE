e08f49afe97a36469283d957eaaad6eab22b6f6d
Sohyunnnn <thgus05061@naver.com>
2025-01-23 20:06:19 +0900

    feat: 파트 클릭 시 input 창 보이도록 기능 추가

--- src/pages/CreatePot/CreatePot.style.ts
+++ src/pages/CreatePot/CreatePot.style.ts
@@ -134,3 +134,42 @@ export const StyledPickersLayout = styled(PickersLayout)<
     fontSize: "1.6rem",
   },
 });
+
+export const inputContainer = (visible: boolean) => css`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+  margin-left: 1.2rem;
+  opacity: ${visible ? 1 : 0};
+`;
+
+export const countInputStyle = css`
+  border: 1px solid ${theme.color.object.alternative};
+  border-radius: 32px;
+  width: 4.7rem;
+  padding: 0.8rem;
+  text-align: center;
+  &:focus {
+    border: 1px solid ${theme.color.point.hero};
+    outline: none;
+  }
+`;
+
+export const partContainer = css`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  gap: 1.6rem 1.2rem;
+  margin-left: 3rem;
+`;
+
+export const partButtonContainer = css`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const partStyle = css`
+  display: flex;
+  color: ${theme.color.base.darkgray};
+  display: flex;
+  ${theme.font.caption3}
+`;
