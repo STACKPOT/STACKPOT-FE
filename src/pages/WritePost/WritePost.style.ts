2c4ea1df5a0b38b361a4b8fe15706d3cb7bc9300
숨 <stacycho0728@naver.com>
2025-01-25 03:22:52 +0900

    style: 글쓰기 페이지 스타일 변경

--- src/pages/WritePost/WritePost.style.ts
+++ src/pages/WritePost/WritePost.style.ts
@@ -2,11 +2,15 @@ import { css } from "@emotion/react";
 import theme from "@styles/theme";
 
 export const container = css`
-  margin-top: 5.55rem;
-  overflow: hidden;
-  gap: 3.2rem;
+  margin-top: 4.8rem;
   display: flex;
   flex-direction: column;
+`;
+export const toastStyle = css`
+  transform: translate(-50%);
+  top: 8.3rem;
+  position: absolute;
+  left: 50%;
 `;
 
 export const content = css`
@@ -15,20 +19,8 @@ export const content = css`
   gap: 3.2rem;
 `;
 
-export const contentBody = css`
-  padding: 3.2rem 10rem;
-  box-shadow: 0px 4px 12px rgba(13, 10, 44, 0.06);
-  border-radius: 3.2rem;
-  border: 1px solid ${theme.color.border.alternative};
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-  flex-direction: column;
-  gap: 3.2rem;
-`;
-
 export const contentTitle = css`
-  ${theme.font.bodyBold2};
+  ${theme.font.body3};
   color: ${theme.color.base.darkgray};
   display: flex;
   align-items: center;
@@ -41,10 +33,45 @@ export const iconStyle = css`
   height: 2.4rem;
 `;
 
+export const buttonContainer = css`
+  display: flex;
+  margin-left: auto;
+`;
+
+export const contentBody = css`
+  padding: 6rem 10rem;
+  box-shadow: 0px 4px 12px rgba(13, 10, 44, 0.06);
+  border-radius: 3.2rem;
+  border: 1px solid ${theme.color.border.alternative};
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 export const titleText = css`
   width: 70.8rem;
   height: 5.2rem;
   padding: 1.2rem 1.6rem;
+`;
+
+export const inputStyle = css`
+  width: 70.8rem;
+  padding: 1.2rem 1.6rem;
+  ${theme.font.caption3}
+  display: flex;
+  flex-grow: 1;
+  border-radius: 8px;
+  color: ${theme.color.base.darkgray};
+  border: 1px solid ${theme.color.object.alternative};
+  &::placeholder {
+    color: ${theme.color.interactive.inactive};
+  }
+  &:focus {
+    border-color: ${theme.color.point.hero};
+    outline: none;
+  }
 `;
 
 export const textareaStyle = css`
@@ -53,7 +80,7 @@ export const textareaStyle = css`
   resize: none;
   width: 70.8rem;
   height: 34.9rem;
-  border: 1px solid ${theme.color.border.alternative};
+  border: 1px solid ${theme.color.object.alternative};
   border-radius: 0.8rem;
   box-sizing: border-box;
   overflow: auto;
@@ -66,6 +93,20 @@ export const textareaStyle = css`
 
   &:focus {
     border-color: ${theme.color.point.hero};
-    border-width: 2px;
+    outline: none;
   }
+`;
+
+export const categoryContainer = css`
+  ${theme.font.caption3};
+  color: ${theme.color.base.darkgray};
+  gap: 3rem;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const categories = css`
+  display: flex;
+  flex-direction: row;
+  gap: 2.4rem;
 `;
--- src/pages/WritePost/WritePost.tsx
+++ src/pages/WritePost/WritePost.tsx
@@ -1,34 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   container,
   contentTitle,
   content,
   iconStyle,
   contentBody,
-  titleText,
   textareaStyle,
+  categoryContainer,
+  categories,
+  buttonContainer,
+  inputStyle,
+  toastStyle,
 } from "./WritePost.style";
 import { PotIcon } from "@assets/svgs";
-import { SelectableField, SignUpButton, TextField } from "@components/index";
-import { toast } from "react-toastify";
+import { Button, CategoryButton } from "@components/index";
+import { partMap } from "@constants/categories";
+import UploadToast from "@components/commons/Toast/UploadToast";
 
 const WritePost: React.FC = () => {
+  const [selectedParts, setSelectedParts] = useState<string[]>([]);
+
+  const [visibleInputs, setVisibleInputs] = useState<{
+    [key: string]: boolean;
+  }>({});
+
+  const [showToast, setShowToast] = useState(false);
+
+  const handleUploading = () => {
+    setShowToast(true);
+    setTimeout(() => {
+      setShowToast(false);
+    }, 2000);
+  };
+
+  const handlePartClick = (partName: string) => {
+    setSelectedParts((prev) =>
+      prev.includes(partName)
+        ? prev.filter((item) => item !== partName)
+        : [...prev, partName]
+    );
+    setVisibleInputs((prev) => ({
+      ...prev,
+      [partName]: !prev[partName],
+    }));
+  };
+
   return (
     <main>
+      {showToast && (
+        <div css={toastStyle}>
+          <UploadToast />
+        </div>
+      )}
       <div css={container}>
         <div css={content}>
           <div css={contentTitle}>
-            <p>게시물 작성하기</p>
+            피드 작성하기
             <PotIcon css={iconStyle} />
+            <div css={buttonContainer}>
+              <Button style="action" onClick={handleUploading}>
+                피드 업로드
+              </Button>
+            </div>
           </div>
-          <SignUpButton>게시물 업로드</SignUpButton>
+
           <div css={contentBody}>
-            <TextField placeholder="메인 제목 작성" inputCss={titleText} />
+            <input css={inputStyle} placeholder="메인 제목 작성" />
             <textarea
               css={textareaStyle}
               placeholder="나의 열정을 이야기해봐요"
             />
-            <SelectableField />
+            <div css={categoryContainer}>
+              카테고리
+              <div css={categories}>
+                {Object.keys(partMap).map((partName) => (
+                  <div key={partName} css={categories}>
+                    <CategoryButton
+                      style={partMap[partName]}
+                      selected={selectedParts.includes(partName)}
+                      onClick={() => handlePartClick(partName)}
+                    >
+                      {partName}
+                    </CategoryButton>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
