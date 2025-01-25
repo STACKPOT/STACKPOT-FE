54e2a70d901c66ef5dee32c02a27449cab45c30c
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-25 15:59:20 +0900

    asset: svg index 수정

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -13,10 +13,8 @@ export { default as SetUpIcon } from "./setUp.svg?react";
 export { default as CloseIcon } from "./close.svg?react";
 export { default as CheckIcon } from "./check.svg?react";
 export { default as SearchIcon } from "./search.svg?react";
-export { default as LeftIcon } from "./chevronLeft.svg?react"
-export { default as PostIconRight } from "./postbtnRight.svg?react"
-export { default as PostIconLeft } from "./postbtnLeft.svg?react"
-export { default as ArrowLeftRoundIcon } from "./arrowLeftRound.svg?react"
+export { default as LeftIcon } from "./chevronLeft.svg?react";
+export { default as ArrowLeftRoundIcon } from "./arrowLeftRound.svg?react";
 export { default as PartyIcon } from "./party.svg?react";
 export { default as PolygonIcon } from "./polygon.svg?react";
 export { default as FireIcon } from "./fire.svg?react";
