13de80563b1b970d30d1fcdc1dd786618077bdb9
숨 <stacycho0728@naver.com>
2025-01-25 23:51:28 +0900

    asset: 수정하기 드롭다운 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -18,3 +18,4 @@ export { default as PencilIcon } from "./pencil.svg?react";
 export { default as PartyIcon } from "./party.svg?react";
 export { default as PolygonIcon } from "./polygon.svg?react";
 export { default as FireIcon } from "./fire.svg?react";
+export { default as DropDownIcon } from "./dropdownIcon.svg?react";
