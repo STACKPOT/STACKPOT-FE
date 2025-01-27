7f122d98f8d3498a54c15b0899962085a483c619
Sohyunnnn <thgus05061@naver.com>
2025-01-27 16:22:04 +0900

    feat: 캘린더 페이지 퍼블리싱

---
+++ src/pages/MyPot/MyPotCalendar/MyPotCalendar.style.ts
@@ -0,0 +1,96 @@
+import { css } from "@emotion/react";
+import theme from "@styles/theme";
+
+export const container = css`
+  display: flex;
+  gap: 7.4rem;
+`;
+
+export const titleContainer = css`
+  ${theme.font.bodyBold2}
+  margin: 4.8rem 0;
+`;
+
+export const titleStyle = css`
+  display: flex;
+  gap: 7.4rem;
+`;
+
+export const calendarStyle = css`
+  padding: 6.5rem 5rem;
+  border-radius: 24px;
+  border: 1px solid ${theme.color.object.alternative};
+  box-shadow: 0px 4px 12px 0px rgba(13, 10, 44, 0.06);
+`;
+
+export const taskContainer = css`
+  padding: 3.2rem 2.4rem;
+  height: 58.8rem;
+
+  border-radius: 24px;
+  border: 1px solid ${theme.color.object.alternative};
+  box-shadow: 0px 4px 12px 0px rgba(13, 10, 44, 0.06);
+`;
+
+export const datePickerStyle = css`
+  .mbsc-ios.mbsc-datepicker-inline {
+    border-color: white;
+  }
+`;
+
+export const dateStyle = css`
+  ${theme.font.title1}
+  text-align: center;
+`;
+
+export const dividerStyle = css`
+  height: 1px;
+  width: 100%;
+  background-color: ${theme.color.object.alternative};
+  margin: 1.6rem 0;
+`;
+
+export const taskBoxStyle = css`
+  padding: 0.8rem 0;
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+export const taskTitleContainer = css`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+`;
+
+export const contentStyle = css`
+  ${theme.font.caption1};
+  color: ${theme.color.object.assistive};
+  height: 2.7rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+export const taskTitleStyle = css`
+  ${theme.font.bodyBold1}
+  color: ${theme.color.base.darkgray}
+`;
+
+export const memberContainer = css`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const profileContainer = css`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+`;
+
+export const nickNameStyle = css`
+  ${theme.font.caption1}
+  color: ${theme.color.object.assistive}
+`;
--- src/pages/MyPot/MyPotCalendar/MyPotCalendar.tsx
+++ src/pages/MyPot/MyPotCalendar/MyPotCalendar.tsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import { useState } from "react";
+import "@mobiscroll/react/dist/css/mobiscroll.min.css";
+import { Datepicker, setOptions, localeKo } from "@mobiscroll/react";
+import {
+  calendarStyle,
+  container,
+  contentStyle,
+  datePickerStyle,
+  dateStyle,
+  dividerStyle,
+  memberContainer,
+  nickNameStyle,
+  profileContainer,
+  taskBoxStyle,
+  taskContainer,
+  taskTitleContainer,
+  taskTitleStyle,
+  titleContainer,
+  titleStyle,
+} from "./MyPotCalendar.style";
+import { DdayBadge, MemberGroup } from "@components/index";
+import { Profile } from "@assets/svgs";
+import { CarrotProfile, MushRoomProfile, OnionProfile } from "@assets/images";
+
+setOptions({
+  locale: localeKo,
+  themeVariant: "light",
+});
 
 const MyPotCalendar = () => {
-  return (
-    <main>MyPotCalendar</main>
-  )
-}
+  const [date, setDate] = useState<Date | null>(null);
 
-export default MyPotCalendar
+  const myMarked = [
+    { date: "2025-02-04" },
+    { date: "2025-01-02T00:00", color: "#46c4f3" },
+    { date: "2025-01-02T00:00", color: "#46c4f3" },
+    { date: "2025-01-06T00:00", color: "#7e56bd" },
+    { date: "2025-01-11T00:00", color: "#7e56bd" },
+    { date: "2025-01-19T00:00", color: "#89d7c9" },
+    { date: "2025-01-28T00:00", color: "#ea4986" },
+  ];
+
+  const getDayOfWeek = (date: Date | null) => {
+    if (!date) return "";
+    const days = [
+      "일요일",
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일",
+    ];
+    return days[date.getDay()];
+  };
+
+  return (
+    <main>
+      <div css={titleContainer}>
+        <p css={titleStyle}>캘린더</p>
+      </div>
+      <div css={container}>
+        <div css={calendarStyle}>
+          <div className="mbsc-grid" css={datePickerStyle}>
+            <Datepicker
+              display="inline"
+              marked={myMarked}
+              value={date}
+              onChange={(e) => setDate(e.value)}
+            />
+          </div>
+        </div>
+        <div css={taskContainer}>
+          <p css={dateStyle}>
+            {date
+              ? `${date.getFullYear()}. ${String(date.getMonth() + 1).padStart(
+                  2,
+                  "0"
+                )}. ${String(date.getDate()).padStart(2, "0")} (${getDayOfWeek(
+                  date
+                )})`
+              : ""}
+          </p>
+          <div css={dividerStyle} />
+          <div css={taskBoxStyle}>
+            <div css={taskTitleContainer}>
+              <DdayBadge days={3} />
+              <p css={taskTitleStyle}>와이어프레임 완료</p>
+            </div>
+            <p css={contentStyle}>
+              와이어 프레임 제작해서 넘기기 두줄와이어 프레임 제작해서 넘기기
+              두줄와이어 프레임 제작해서 넘기기 두줄와이어 프레임 제작와이어
+              프레임 제작해서 넘기기 두줄와이어 프레임 제작해서 넘기기
+              두줄와이어 프레임 제작해서 넘기기 두줄와이어 프레임 제작...
+            </p>
+            <div css={memberContainer}>
+              <div css={profileContainer}>
+                <Profile />
+                <p css={nickNameStyle}>닉네임</p>
+              </div>
+              <MemberGroup
+                profileImageList={[
+                  MushRoomProfile,
+                  CarrotProfile,
+                  OnionProfile,
+                ]}
+              />
+            </div>
+          </div>
+          <div css={dividerStyle} />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default MyPotCalendar;
