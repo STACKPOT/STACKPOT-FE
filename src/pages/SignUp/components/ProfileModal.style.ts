478ef22e1f0781de6d8d0a16cb84e8eeba4d5ee8
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-28 22:42:18 +0900

    refactor: 불필요한 코드 삭제

--- src/pages/SignUp/components/ProfileModal.style.ts
+++ src/pages/SignUp/components/ProfileModal.style.ts
@@ -1,17 +1,6 @@
 import { css } from "@emotion/react";
 import theme from "@styles/theme";
 
-export const modalBackgroundContainer =css`
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    position: fixed;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(0, 0, 0, 0.40);
-`
 export const profileContainer = css`
     display: flex;
     flex-direction: column;
