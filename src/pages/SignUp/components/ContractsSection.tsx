681210bc246ab730b2063f5e245943ed5add3983
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-01-28 21:49:39 +0900

    refactor: 이용약관 체크박스로 변경

--- src/pages/SignUp/components/ContractsSection.tsx
+++ src/pages/SignUp/components/ContractsSection.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { contractContainer, container, detailButtonStyle, contractStyle } from "./ContractsSection.style";
 import ContractModal from "./ContractModal";
+import { CheckBox } from "@components/index";
 
 interface ContractsSectionProps {
     onAgree: (agreed: boolean) => void;
 }
 const ContractsSection: React.FC<ContractsSectionProps> = ({ onAgree }: ContractsSectionProps) => {
     const [contracts, setContracts] = useState<{ agreed: boolean, preview: string, type: "service" | "privacy" }[]>([
-        { agreed: true, preview: "서비스 약관에 동의합니다", type: "service" },
-        { agreed: true, preview: "개인정보 수집 및 이용에 동의합니다.", type: "privacy" }
+        { agreed: false, preview: "서비스 약관에 동의합니다", type: "service" },
+        { agreed: false, preview: "개인정보 수집 및 이용에 동의합니다.", type: "privacy" }
     ]);
     const [contractModal, setContractModal] = useState<typeof contracts[0] | null>(null);
 
@@ -28,7 +29,7 @@ const ContractsSection: React.FC<ContractsSectionProps> = ({ onAgree }: Contract
             <div css={container}>
                 {contracts.map((contract, i) =>
                     <div css={contractContainer} >
-                        <button onClick={() => handleAgree(i)}></button>
+                        <CheckBox selected={contract.agreed} onSelect={() => handleAgree(i)}></CheckBox>
                         <p css={contractStyle}>{contract.preview}</p>
                         <button css={detailButtonStyle} onClick={() => setContractModal(contract)}>내용보기</button>
                     </div>
