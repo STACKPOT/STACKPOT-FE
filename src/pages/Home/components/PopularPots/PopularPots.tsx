029f4cc4c1e3daea1de91f904261e92901b0d919
Sohyunnnn <thgus05061@naver.com>
2025-02-03 16:13:34 +0900

    feat: 페이지네이션 버튼 수정 및 api 연결

--- src/pages/Home/components/PopularPots/PopularPots.tsx
+++ src/pages/Home/components/PopularPots/PopularPots.tsx
@@ -1,36 +1,87 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
-import potCardsData from "mocks/potCardsData";
-import { swiperContainer } from "./PopularPots.style";
+import {
+  buttonContainer,
+  buttonStyle,
+  iconStyle,
+  pageNumberStyle,
+  swiperContainer,
+} from "./PopularPots.style";
 import { PotCard } from "@components/index";
+import useGetPots from "apis/hooks/pots/useGetPots";
+import { useState } from "react";
+import { LeftIcon } from "@assets/svgs";
 
 const PopularPots = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data } = useGetPots({ page: currentPage, size: 3 });
+
+  const totalPages = data?.totalPages || 1;
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
-    <Swiper
-      css={swiperContainer}
-      modules={[Pagination, Navigation]}
-      centeredSlides={false}
-      spaceBetween={16}
-      slidesPerView={3}
-      slidesPerGroup={3}
-      pagination={{
-        type: "fraction",
-      }}
-      navigation={true}
-    >
-      {potCardsData.map((card) => (
-        <SwiperSlide key={card.id}>
-          <PotCard
-            profileImage={card.profileImage}
-            nickname={card.nickname}
-            dday={card.dday}
-            title={card.title}
-            content={card.content}
-            categories={card.categories}
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      {!data?.pots?.length ? (
+        <p>데이터가 없습니다.</p>
+      ) : (
+        <>
+          <Swiper
+            css={swiperContainer}
+            modules={[Pagination, Navigation]}
+            centeredSlides={false}
+            spaceBetween={16}
+            slidesPerView={3}
+            slidesPerGroup={3}
+            navigation={true}
+          >
+            {data.pots.map((pot) => (
+              <SwiperSlide key={pot.userId}>
+                <PotCard
+                  role={pot.userRole}
+                  nickname={pot.userNickname}
+                  dday={pot.dday}
+                  title={pot.potName}
+                  content={pot.potContent}
+                  categories={pot.recruitmentRoles}
+                />
+              </SwiperSlide>
+            ))}
+            <div css={buttonContainer}>
+              <button
+                type="button"
+                css={buttonStyle}
+                onClick={handlePrevPage}
+                disabled={currentPage === 1}
+              >
+                <LeftIcon />
+              </button>
+              <p css={pageNumberStyle}>
+                {currentPage} / {totalPages}
+              </p>
+              <button
+                type="button"
+                css={buttonStyle}
+                onClick={handleNextPage}
+                disabled={currentPage === totalPages}
+              >
+                <LeftIcon css={iconStyle} />
+              </button>
+            </div>
+          </Swiper>
+        </>
+      )}
+    </>
   );
 };
 
