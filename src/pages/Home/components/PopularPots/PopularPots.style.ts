92c147176f169f9221d40ff5f92f62706d2f2bc8
Sohyunnnn <thgus05061@naver.com>
2025-02-03 16:13:18 +0900

    feat: 페이지네이션 스타일 변경

--- src/pages/Home/components/PopularPots/PopularPots.style.ts
+++ src/pages/Home/components/PopularPots/PopularPots.style.ts
@@ -5,40 +5,34 @@ export const swiperContainer = css`
   position: relative;
   overflow: hidden;
   width: 84.8rem;
-  height: 29.9rem;
-
-  .swiper-pagination {
-    ${theme.font.body2}
-    color: ${theme.color.object.assistive};
-    bottom: 10px;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 5rem;
-  }
 
   .swiper-button-next,
   .swiper-button-prev {
-    opacity: 1 !important;
-    width: 4.4rem;
-    background-color: ${theme.color.interactive.disable};
-    border-radius: 50%;
-    display: flex;
-    cursor: pointer;
-    transform: translateY(290%);
+    display: none;
   }
+`;
 
-  .swiper-button-next {
-    right: calc(50% - 95px);
-  }
+export const buttonContainer = css`
+  display: flex;
+  gap: 3.2rem;
+  margin-top: 1.6rem;
+  justify-content: center;
+  align-items: center;
+`;
 
-  .swiper-button-prev {
-    left: calc(50% - 95px);
-  }
+export const pageNumberStyle = css`
+  ${theme.font.body2}
+  color: ${theme.color.object.assistive};
+`;
 
-  .swiper-button-next::after,
-  .swiper-button-prev::after {
-    font-size: 1.2rem;
-    color: ${theme.color.interactive.inactive};
-    font-weight: bold;
-  }
+export const buttonStyle = css`
+  padding: 1rem;
+  border-radius: 50%;
+  border: none;
+  background-color: ${theme.color.object.normal};
+  height: 4.4rem;
+`;
+
+export const iconStyle = css`
+  transform: scaleX(-1);
 `;
