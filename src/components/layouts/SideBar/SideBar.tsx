fac6ca1c723b7e85e2a5a482011cd42d8668acd1
Sohyunnnn <thgus05061@naver.com>
2025-02-04 23:38:54 +0900

    style: 스타일 수정

--- src/components/layouts/SideBar/SideBar.tsx
+++ src/components/layouts/SideBar/SideBar.tsx
@@ -29,7 +29,6 @@ const menuItems = [
 const SideBar: React.FC = () => {
   const [top, setTop] = useState(0);
   const [footerTop, setFooterTop] = useState<number | null>(null);
-  const [profileModal, setProfileModal] = useState<boolean>(false);
 
   useEffect(() => {
     const initialTop = window.innerHeight / 2 + window.scrollY;
