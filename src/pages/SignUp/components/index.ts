9853640b15ddff029ad56f2e043dac93e27ee599
Sohyunnnn <thgus05061@naver.com>
2025-02-06 01:08:17 +0900

    remove: 파일 제거

--- src/pages/SignUp/components/index.ts
+++ src/pages/SignUp/components/index.ts
@@ -1,6 +1,5 @@
 import CategorySelection from "./CategorySelection/CategorySelection";
 import ContractModal from "./ContractModal/ContractModal";
-import NicknameInput from "./NicknameInput";
 import ProfileModal from "./ProfileModal/ProfileModal";
 import Section from "./Section/Section";
 import ContractsSection from "./ContractsSection/ContractsSection";
@@ -9,7 +8,6 @@ import ContractionDetailList from "./ContractionDetailList/ContractDetailList";
 export {
   CategorySelection,
   ContractModal,
-  NicknameInput,
   ProfileModal,
   Section,
   ContractsSection,
