c9b50989208a93d83741857658d70bc7d897e028
숨 <stacycho0728@naver.com>
2025-02-07 19:54:17 +0900

    feat: 마이페이지 get 연동

--- src/apis/mypageAPI.ts
+++ src/apis/mypageAPI.ts
@@ -2,5 +2,5 @@ import { authApiGet } from "./apiUtils";
 import { GetMyPageParams, MyPageResponse } from "./types/mypage";
 
 export const GetMyPage = async ({ dataType }: GetMyPageParams) => {
-  return authApiGet<MyPageResponse>("/users/mypages", dataType);
+  return authApiGet<MyPageResponse>("/users/mypages", { dataType });
 };
