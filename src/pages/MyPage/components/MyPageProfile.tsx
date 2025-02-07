a300997667e0ed5ce6c7add0fbfa242a6608c340
숨 <stacycho0728@naver.com>
2025-02-07 21:48:14 +0900

    refactor: roleimage constants 이용

--- src/pages/MyPage/components/MyPageProfile.tsx
+++ src/pages/MyPage/components/MyPageProfile.tsx
@@ -12,29 +12,17 @@ import TemperatureBar from "@components/commons/TemperatureBar/TemperatureBar";
 import { useNavigate } from "react-router-dom";
 import routes from "@constants/routes";
 import useGetMyPage from "apis/hooks/mypage/useGetMyPage";
-import { MushroomImage } from "@assets/images";
+import { roleImages } from "@constants/roleImage";
 
 const MyPageProfile: React.FC = () => {
   const navigate = useNavigate();
-  const { data, isLoading, error } = useGetMyPage({ dataType: "pot" });
-
-  if (isLoading) {
-    return (
-      <p style={{ textAlign: "center", marginTop: "20px" }}>로딩 중입니다...</p>
-    );
-  }
-
-  if (error) {
-    return (
-      <p style={{ color: "red", textAlign: "center", marginTop: "20px" }}>
-        에러가 발생했습니다: {error.message}
-      </p>
-    );
-  }
+  const { data } = useGetMyPage({ dataType: "pot" });
 
   const nickname = data?.nickname || "사용자";
   const userIntroduction = data?.userIntroduction || "소개가 없습니다.";
   const userTemperature = data?.userTemperature ?? 0;
+  const role = data?.role || "FRONTEND";
+  const profileImage = roleImages[role];
 
   const handleSetUp = () => {
     navigate(routes.setting);
@@ -42,7 +30,7 @@ const MyPageProfile: React.FC = () => {
 
   return (
     <div css={profileContainer}>
-      <img css={profileStyle} src={MushroomImage} alt="프로필 이미지" />
+      <img css={profileStyle} src={profileImage} alt="프로필 이미지" />
       <div css={contentContainer}>
         <div css={nicknameContainer}>
           <h1 css={nicknameStyle}>{nickname}</h1>
