5929e4662280b619bbb37c35a47fdf91c5be22be
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-02-10 01:35:55 +0900

    feat: 플로팅 버튼 리팩토링 및 팟 메인에 추가

--- src/components/commons/FloatingButton/FloatingButton.tsx
+++ src/components/commons/FloatingButton/FloatingButton.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { iconStyle, WriteButton } from "./FloatingButton.style";
-import { PencilIcon } from "@assets/svgs";
-const FloatingButton: React.FC = () => {
+import { AddIcon, PencilIcon } from "@assets/svgs";
+import routes from "@constants/routes";
+
+interface FloatingButtonProps {
+  type: "feed" | "pot";
+}
+
+const FloatingButton: React.FC<FloatingButtonProps> = ({ type = "feed" }: FloatingButtonProps) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate("/writing-page");
+    if (type === "feed") {
+      navigate("/writing-page");
+    } else {
+      navigate(routes.createPot);
+    }
   };
 
   return (
     <div css={WriteButton} onClick={handleClick}>
-      <PencilIcon css={iconStyle} />
-      피드 작성
+      {type === "feed" ? <PencilIcon css={iconStyle} /> : <AddIcon />}
+      {type === "feed" ? "피드 작성" : "팟 만들기"}
     </div>
   );
 };
