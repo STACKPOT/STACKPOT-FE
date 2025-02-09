3292d9e7a3e5552412329fc1f774b7d6dff0d0c1
숨 <stacycho0728@naver.com>
2025-02-09 20:17:19 +0900

    refactor: modal를 mypage로 이동

--- src/apis/types/mypage.ts
+++ src/apis/types/mypage.ts
@@ -39,3 +39,20 @@ interface MyBadges {
 export interface GetMyPageParams {
   dataType: string;
 }
+
+export interface FinishedModalResponse {
+  potId: number;
+  potName: string;
+  userId: number;
+  potStartDate: string;
+  potEndDate: string;
+  potContent: string;
+  potStatus: string;
+  potSummary: string;
+  appealContent: string;
+  userPotRole: string;
+}
+
+export interface GetFinishedModalParams {
+  potId: number;
+}
