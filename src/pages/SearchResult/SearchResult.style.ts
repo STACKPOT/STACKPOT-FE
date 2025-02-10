e837f9eecedc7d1d9ce8db0c2a6d3587badeaa75
Sohyunnnn <thgus05061@naver.com>
2025-02-11 02:11:41 +0900

    style: 스타일 수정

--- src/pages/SearchResult/SearchResult.style.ts
+++ src/pages/SearchResult/SearchResult.style.ts
@@ -33,6 +33,6 @@ export const buttonContainer = css`
 
 export const gridContainer = css`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(3, 27.2rem);
   gap: 3.2rem;
 `;
