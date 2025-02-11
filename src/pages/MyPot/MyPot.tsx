06a9d2255e93a729aeb9736d29fc0366dc43c4c2
Sohyunnnn <thgus05061@naver.com>
2025-02-11 22:56:16 +0900

    refactor: 나의 팟 메인 페이지 리팩토링 및 스타일 수정

--- src/pages/MyPot/MyPot.tsx
+++ src/pages/MyPot/MyPot.tsx
@@ -1,11 +1,11 @@
 import { PotIcon } from "@assets/svgs";
 import {
   container,
-  content,
-  contentBody,
   contentTitle,
   description,
   iconStyle,
+  onGoningPotContainer,
+  textContainer,
 } from "./MyPot.style";
 import { OnGoingPotCard } from "@components/index";
 import useGetMyPot from "apis/hooks/myPots/useGetMyPot";
@@ -14,34 +14,28 @@ const MyPot: React.FC = () => {
   const { data } = useGetMyPot();
 
   return (
-    <main>
-      <div css={container}>
-        <div css={content}>
-          <div css={contentTitle}>
-            <p>나의 팟</p>
-            <PotIcon css={iconStyle} />
-          </div>
-
-          <div css={contentBody}>
-            <p css={description}>
-              내가 입장한 팟을 모았어요. 클릭한 뒤 각각의 팟에서 업무를 시작해
-              보세요.
-            </p>
-            {data && data.length > 0 ? (
-              data.map((pot) => (
-                <OnGoingPotCard
-                  key={pot.potId}
-                  id={pot.potId}
-                  isMyPot={pot.isOwner}
-                  title={pot.potName}
-                  memberList={Object.keys(pot.members)}
-                />
-              ))
-            ) : (
-              <div>데이터가 없습니다.</div>
-            )}
-          </div>
-        </div>
+    <main css={container}>
+      <div css={contentTitle}>
+        <p>나의 팟</p>
+        <PotIcon css={iconStyle} />
+      </div>
+      <p css={description}>
+        내가 입장한 팟을 모았어요. 클릭한 뒤 각각의 팟에서 업무를 시작해 보세요.
+      </p>
+      <div css={onGoningPotContainer}>
+        {data && data.length > 0 ? (
+          data.map((pot) => (
+            <OnGoingPotCard
+              key={pot.potId}
+              id={pot.potId}
+              isMyPot={pot.isOwner}
+              title={pot.potName}
+              memberList={Object.keys(pot.members)}
+            />
+          ))
+        ) : (
+          <p css={textContainer}>데이터가 없습니다.</p>
+        )}
       </div>
     </main>
   );
