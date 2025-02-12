f0f33e90a5eeac88ac593680d33aeb9b490bde9d
DeoKanKoong <rla9046@gmail.com>
2025-02-12 15:05:48 +0900

    type: 타입 추가

--- src/apis/types/myPot.ts
+++ src/apis/types/myPot.ts
@@ -1,5 +1,5 @@
 export interface Todo {
-  todoId: number;
+  todoId: number | null;
   content: string;
   status: string;
 }
@@ -26,13 +26,30 @@ export interface GetTodoParams {
   size: number;
 }
 
-export interface TodoUpdateRequest {
-  potId: number;
-  data: TodoItem[];
+export type APITaskStatus = "OPEN" | "IN_PROGRESS" | "CLOSED";
+
+export interface Participant {
+  potMemberId: number;
+  userId: number;
+  nickName: string;
+  role: string;
 }
 
-export interface TodoItem {
-  todoId: number | null;
-  content: string;
-  status: string;
+export interface Task {
+  taskboardId: number;
+  title: string;
+  description: string;
+  creatorNickname: string;
+  creatorRole: string;
+  category: string[];
+  status: APITaskStatus;
+  deadLine: string;
+  participants: Participant[];
+  dday: string;
+}
+
+export interface TaskResponse {
+  OPEN?: Task[];
+  IN_PROGRESS?: Task[];
+  CLOSED?: Task[];
 }
