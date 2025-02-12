478618d2a6ec32c4b5b5eddb96ede739a695c47c
Sohyunnnn <thgus05061@naver.com>
2025-02-13 00:56:58 +0900

    feat: 피드 상세 퍼블리싱

--- src/pages/FeedDetail/FeedDetail.tsx
+++ src/pages/FeedDetail/FeedDetail.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { LeftIcon } from "@assets/svgs";
+import {
+  contentStyle,
+  dateStyle,
+  dividerStyle,
+  headerContainer,
+  iconStyle,
+  informationContainer,
+  mainContainer,
+  nicknameStyle,
+  profileContainer,
+  titleContainer,
+  titleStyle,
+} from "./FeedDetail.style";
 
 const FeedDetail = () => {
   return (
-    <div>FeedDetail</div>
-  )
-}
+    <main css={mainContainer}>
+      <div css={headerContainer}>
+        <div css={titleContainer}>
+          <LeftIcon css={iconStyle} />
+          <h1 css={titleStyle}>개발자 입문자를 위한 안내 글을 써보았다.</h1>
+        </div>
+        <div css={profileContainer}>
+          <div>버섯</div>
+          <div css={informationContainer}>
+            <p css={nicknameStyle}>닉네임</p>
+            <p css={dateStyle}>날짜</p>
+          </div>
+        </div>
+        <div css={dividerStyle} />
+      </div>
+      <div css={contentStyle}>본문내용</div>
+    </main>
+  );
+};
 
-export default FeedDetail
+export default FeedDetail;
