14cbda5bc609a57f0d5b5d82387ceb576bcbf30f
DeoKanKoong <rla9046@gmail.com>
2025-02-15 17:15:29 +0900

    rename: 변수 이름 MyPot 추가

--- src/apis/myPotAPI.ts
+++ src/apis/myPotAPI.ts
@@ -1,34 +1,34 @@
 import { authApiGet, authApiPatch, authApiDelete } from "./apiUtils";  
-import { Result, Todo, TaskResponse, MyPotResponse, TaskDetailResponse, TaskPatch } from "./types/myPot"; 
+import { Result, Todo, TaskResponse, MyPotResponse, TaskDetailResponse, TaskPatch, TaskAPIPrams } from "./types/myPot"; 
 
-export const getTodo = async (potId: number, page: number, size: number) => {
+export const getMyPotTodo = async (potId: number, page: number, size: number) => {
   const params = { page, size };
   return authApiGet<Result>(`/my-pots/${potId}/todos`, params);
 };
 
-export const patchTodo = async ({ potId, data }: { potId: number; data: Todo[] })  => {
+export const patchMyPotTodo = async ({ potId, data }: { potId: number; data: Todo[] })  => {
   return authApiPatch<Result>(`/my-pots/${potId}/todos`, data);
 };
 
-export const patchMyTodoStatus = async (potId: number, todoId: number) => {
+export const patchMyPotTodoStatus = async (potId: number, todoId: number) => {
   return authApiPatch(`/my-pots/${potId}/todos/${todoId}`, { status: "COMPLETED" });
 };
 
-export const getTask = async (potId: number) => {
+export const getMyPotTask = async (potId: number) => {
   return authApiGet<TaskResponse>(`/my-pots/${potId}/tasks`);
 };
 
-export const getTaskDetail = async (potId: number, taskId: number) => {
+export const getMyPotTaskDetail = async ({ potId, taskId }: TaskAPIPrams) => {
   return authApiGet<TaskDetailResponse>(`/my-pots/${potId}/tasks/${taskId}`);
 };
 
-export const deleteTask = async (potId: number, taskId: number) => {
+export const deleteMyPotTask = async ({ potId, taskId }: TaskAPIPrams) => {
   return authApiDelete(`/my-pots/${potId}/tasks/${taskId}`);
 };
 
-export const patchTask = async (potId: number, taskId: number, data: TaskPatch) => {
+export const patchMyPotTask = async ({ potId, taskId }: TaskAPIPrams, data: TaskPatch) => {
   return authApiPatch(`/my-pots/${potId}/tasks/${taskId}`, data);
-}
+};
 
 export const getMyPot = async () => {
   return authApiGet<MyPotResponse[]>("/my-pots");
