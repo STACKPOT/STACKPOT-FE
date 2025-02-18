ec7f953aae0c93a1300214347a1384d14ddaef97
DeoKanKoong <rla9046@gmail.com>
2025-02-19 04:59:03 +0900

    rename: 오타 수정

--- src/apis/hooks/myPots/useGetMyPotMemeber.ts
+++ src/apis/hooks/myPots/useGetMyPotMemeber.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getMyPotMembers } from "apis/myPotAPI";
 import { GetTaskParams } from "apis/types/myPot";
 
-export const useGetMyPotaMembers = ({ potId }: GetTaskParams) => {
+export const useGetMyPotMembers = ({ potId }: GetTaskParams) => {
   return useQuery({
     queryKey: ["myPotMembers", potId],
     queryFn: () => getMyPotMembers({potId}),
