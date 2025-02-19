e0bd3c68e150de61fa4479f296deb6e711c8884d
DeoKanKoong <rla9046@gmail.com>
2025-02-20 03:49:53 +0900

    refactor: task status 변경 api 연결

--- src/pages/MyPot/MyPotStatus/TaskDetail/TaskDetail.tsx
+++ src/pages/MyPot/MyPotStatus/TaskDetail/TaskDetail.tsx
@@ -31,20 +31,31 @@ import routes from "@constants/routes";
 import useGetMyPotTaskDetail from "apis/hooks/myPots/useGetMyPotTaskDetail";
 import { useDeleteMyPotTask } from "apis/hooks/myPots/useDeleteMyPotTask";
 import { AboutWorkModalWrapper, Loading } from "../../components/index";
-import { TaskStatus } from "types/taskStatus";
+import { APITaskStatus, TaskStatus } from "types/taskStatus";
 import { Role } from "types/role";
 import { roleImages } from "@constants/roleImage";
 import { displayStatus, WorkModal } from "@constants/categories";
 import ConfirmModalWrapper from "@pages/MyPot/components/ConfirmModalWrapper/ConfirmModalWrapper";
+import ChangeStatusModalWrapper from "@pages/MyPot/components/ChangeStatusModal/ChangeStatusModalWrapper";
+import { usePatchMyPotStatus } from "apis/hooks/myPots/usePatchMyPotStatus";
+import { AnotherTaskStatus } from "../../../../types/taskStatus";
 
 const TaskDetailPage: React.FC = () => {
   const { potId, taskId } = useParams<{ potId: string; taskId: string }>();
   const navigate = useNavigate();
 
-  const { data: task, isLoading, error } = useGetMyPotTaskDetail({ potId: Number(potId), taskId: Number(taskId) });
-  const { mutate: deleteTask, isPending } = useDeleteMyPotTask();
+  const potIdNumber = Number(potId);
+  const taskIdNumber = Number(taskId);
+
+  const { data: task, isLoading, error } = useGetMyPotTaskDetail({
+    potId: potIdNumber,
+    taskId: taskIdNumber,
+  });
+  const { mutate: deleteTask, isPending: isDeletePending } = useDeleteMyPotTask();
+  const { mutate: patchStatus, isPending: isStatusPending } = usePatchMyPotStatus();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isChangingModalOpen, setIsChangingModalOpen] = useState(false);
   const [modalTitle, setModalTitle] = useState<string>(WorkModal[0]);
   const [activeStatus, setActiveStatus] = useState<TaskStatus | null>(null);
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
@@ -54,11 +65,10 @@ const TaskDetailPage: React.FC = () => {
   };
 
   const confirmDeleteTask = () => {
-      deleteTask({ potId: Number(potId), taskId: Number(taskId) });
-      setIsConfirmOpen(false);
-      navigate(`${routes.myPot.base}/${routes.task}/${potId}`);
+    deleteTask({ potId: potIdNumber, taskId: taskIdNumber });
+    setIsConfirmOpen(false);
+    navigate(`${routes.myPot.base}/${routes.task}/${potId}`);
   };
-  
   const handleDeleteTask = () => {
     setIsConfirmOpen(true);
   };
@@ -70,14 +80,44 @@ const TaskDetailPage: React.FC = () => {
     setIsModalOpen(true);
   };
 
-  if (isLoading || isPending) return <Loading />;
+  const handleOpenChangingModal = () => {
+    setIsChangingModalOpen(true);
+  };
+
+  const confirmChangeModal = (newStatus: AnotherTaskStatus) => {
+    const reverseDisplayStatus = Object.fromEntries(
+      Object.entries(displayStatus).map(([key, value]) => [value, key])
+    ) as Record<AnotherTaskStatus, APITaskStatus>;
+
+    const patchValue = reverseDisplayStatus[newStatus] as string;
+    console.log(potIdNumber, taskIdNumber, patchValue);
+    patchStatus({
+      potId: potIdNumber,
+      taskId: taskIdNumber,
+      data: { status: patchValue },
+    });
+    setIsChangingModalOpen(false);
+  };
+
+  if (isLoading || isDeletePending || isStatusPending) return <Loading />;
   if (error) return <p>데이터를 불러오는 중 오류가 발생했습니다.</p>;
   if (!task?.result) return <p>데이터를 찾을 수 없습니다.</p>;
 
   return (
     <>
-      <ConfirmModalWrapper isModalOpen={isConfirmOpen} onClose={() => setIsConfirmOpen(false)} onConfirm={confirmDeleteTask} />
-      
+      <ConfirmModalWrapper
+        isModalOpen={isConfirmOpen}
+        onClose={() => setIsConfirmOpen(false)}
+        onConfirm={confirmDeleteTask}
+      />
+
+      <ChangeStatusModalWrapper
+        isModalOpen={isChangingModalOpen}
+        onClose={() => setIsChangingModalOpen(false)}
+        onConfirm={confirmChangeModal}
+        initialStatus={displayStatus[task.result.status]}
+      />
+
       <AboutWorkModalWrapper
         isModalOpen={isModalOpen}
         activeStatus={activeStatus}
@@ -94,7 +134,9 @@ const TaskDetailPage: React.FC = () => {
           <div css={titleStyle}>{task.result.title}</div>
         </div>
         <div css={rightContainer}>
-          <StateBadge content={displayStatus[task.result.status]} />
+          <button type="button" onClick={handleOpenChangingModal}>
+            <StateBadge content={displayStatus[task.result.status]} />
+          </button>
           <div css={dropdownWrapperStyle} onClick={(event) => event.stopPropagation()}>
             <MyFeedDropdown
               topMessage="수정하기"
@@ -106,7 +148,11 @@ const TaskDetailPage: React.FC = () => {
         </div>
       </div>
       <div css={profileContainer}>
-        <img css={profileImgStyle} src={roleImages[task.result.creatorRole as Role] ?? ""} alt={task.result.creatorNickname} />
+        <img
+          css={profileImgStyle}
+          src={roleImages[task.result.creatorRole as Role] ?? ""}
+          alt={task.result.creatorNickname}
+        />
         <span css={nicknameStyle}>{task.result.creatorNickname}</span>
         <DdayBadge days={task.result.dday} />
       </div>
