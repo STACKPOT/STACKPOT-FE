c2affe91f61de2696b199258644213492d1f97bb
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-02-20 02:36:17 +0900

    refactor: 팟 시작하기 후 멤버 모달 안뜨는 문제 해결

--- src/pages/PotDetail/components/MemberKakaoIdModal/MemberKakaoIdModal.tsx
+++ src/pages/PotDetail/components/MemberKakaoIdModal/MemberKakaoIdModal.tsx
@@ -3,6 +3,7 @@ import { closeIconStyle, container, descriptionBlueStyle, descriptionStyle, kaka
 import useGetPotMembers from "apis/hooks/pots/useGetPotMembers";
 import { roleImages } from "@constants/roleImage";
 import { useEffect, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface MemberKakaoIdModalProps {
     potId: number;
@@ -11,6 +12,12 @@ interface MemberKakaoIdModalProps {
 const MemberKakaoIdModal: React.FC<MemberKakaoIdModalProps> = ({ potId, onModalCancel }: MemberKakaoIdModalProps) => {
     const { data: members } = useGetPotMembers(potId);
     const [ownerNickname, setOwnerNickname] = useState<string>("팀장");
+
+    const handleCancelModal = () => {
+        onModalCancel();
+        const queryClient = useQueryClient();
+        queryClient.invalidateQueries({ queryKey: ["potDetail"] })
+    }
 
     useEffect(() => {
         if (members) {
@@ -21,12 +28,12 @@ const MemberKakaoIdModal: React.FC<MemberKakaoIdModalProps> = ({ potId, onModalC
     return (
         <div css={modalBackgroundStyle}>
             <div css={modalStyle}>
-                <CloseIcon css={closeIconStyle} onClick={onModalCancel} type="button" />
+                <CloseIcon css={closeIconStyle} onClick={handleCancelModal} type="button" />
                 <div css={container}>
                     <h1 css={titleStyle}>팀원 카카오톡 아이디를 알려드립니다.</h1>
                     <p css={descriptionStyle}>팀장인 <span css={descriptionBlueStyle}>{ownerNickname}</span>은 업무별 현황 페이지 상단에서 확인 가능합니다.</p>
                     <div css={membersContainer}>
-                        {members && members.filter((member)=>!member.owner).map((member) =>
+                        {members && members.filter((member) => !member.owner).map((member) =>
                             <div css={memberContainer}>
                                 <img css={profileStyle} src={roleImages[member.potRole]} alt="profile" />
                                 <div css={nicknameIdContainer}>
