d6587db975a08d5e94e3049ccb27162475d81dae
DeoKanKoong <rla9046@gmail.com>
2025-02-19 18:00:24 +0900

    refactor: 역할군에 따른 image 배정

--- src/pages/MyPot/components/ContributorList/ContributorList.tsx
+++ src/pages/MyPot/components/ContributorList/ContributorList.tsx
@@ -1,23 +1,34 @@
-import { contributorButtonOuterContainer, contributorButtonStyle, contributorButtonInnerContainer, nicknameStyle, profileImageStyle } from "./ContributorList.style";
+import { 
+  contributorButtonOuterContainer, 
+  contributorButtonStyle, 
+  contributorButtonInnerContainer, 
+  nicknameStyle, 
+  profileImageStyle 
+} from "./ContributorList.style";
 import { useParams } from "react-router-dom";
 import { useGetMyPotMembers } from "apis/hooks/myPots/useGetMyPotMemeber";
-import { MushroomImage } from "@assets/images";
+import { roleImages } from "@constants/roleImage";
+import { Role } from "types/role";
 
 const ContributorList: React.FC = () => {
   const { potId } = useParams<{ potId: string }>();
   const potIdNumber = Number(potId);
   const { data } = useGetMyPotMembers({ potId: potIdNumber });
 
-  return(
+  return (
     <div css={contributorButtonOuterContainer}>
-      {data?.result?.map((member) => (
-        <div key={member.potMemberId} css={contributorButtonStyle(false)}>
-          <div css={contributorButtonInnerContainer}>
-            <img src={MushroomImage} alt="프로필 이미지" css={profileImageStyle} />
-            <div css={nicknameStyle}>{member.nickname}</div>
+      {data?.result?.map((member) => {
+        const profileImage = roleImages[member.potRole as Role] || roleImages.DEFAULT;
+
+        return (
+          <div key={member.potMemberId} css={contributorButtonStyle(false)}>
+            <div css={contributorButtonInnerContainer}>
+              <img src={profileImage} alt="프로필 이미지" css={profileImageStyle} />
+              <div css={nicknameStyle}>{member.nickname}</div>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
