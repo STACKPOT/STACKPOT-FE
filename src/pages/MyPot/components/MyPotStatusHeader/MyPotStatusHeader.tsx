fa14dfd6c39956dc8b3533cf8348339963576e07
DeoKanKoong <rla9046@gmail.com>
2025-02-20 16:21:30 +0900

    QA: 남은 할일 동적 처리

--- src/pages/MyPot/components/MyPotStatusHeader/MyPotStatusHeader.tsx
+++ src/pages/MyPot/components/MyPotStatusHeader/MyPotStatusHeader.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useGetMyPotTodo from "apis/hooks/myPots/useGetMyPotTodo";
 import { MyPotIcon } from "@assets/svgs";
@@ -7,14 +8,20 @@ const MyPotStatusHeader = () => {
   const { potId } = useParams<{ potId: string }>();
   const potIdNumber = Number(potId) || 0;
 
-  const { data } = useGetMyPotTodo({ potId: potIdNumber, page: 1, size: 3 });
+  const { data } = useGetMyPotTodo({ potId: potIdNumber, page: 1, size: 1 });
+
+  const [completedCount, setCompletedCount] = useState(0);
+
+  useEffect(() => {
+    setCompletedCount(data?.todos?.[0]?.todos?.filter(todo => todo.status === "COMPLETED").length ?? 0);
+  }, [data]); 
 
   return (
     <div css={boardStyle}>
       <MyPotIcon css={iconStyle} />
       <div css={boardTextStyle}>
         {data?.title ?? ""}- 안녕하세요, <span css={highlightStyle}>{data?.nickname ?? ""}</span> 님! 오늘 할 일이{" "}
-        <span css={highlightStyle}>{data?.taskCount ?? 0}</span>개 있어요.
+        <span css={highlightStyle}>{(data?.taskCount ?? 0) - completedCount}</span>개 있어요.
       </div>
     </div>
   );
