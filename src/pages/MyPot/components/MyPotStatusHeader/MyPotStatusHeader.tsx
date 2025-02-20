a454f0520c5f4047db3ad3edbef485481c09967c
DeoKanKoong <rla9046@gmail.com>
2025-02-21 01:14:03 +0900

    refactor: 백엔드 데이터 수정본에 맞춰서 todoCount 수정

--- src/pages/MyPot/components/MyPotStatusHeader/MyPotStatusHeader.tsx
+++ src/pages/MyPot/components/MyPotStatusHeader/MyPotStatusHeader.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useGetMyPotTodo from "apis/hooks/myPots/useGetMyPotTodo";
 import { MyPotIcon } from "@assets/svgs";
@@ -10,18 +9,12 @@ const MyPotStatusHeader = () => {
 
   const { data } = useGetMyPotTodo({ potId: potIdNumber, page: 1, size: 1 });
 
-  const [completedCount, setCompletedCount] = useState(0);
-
-  useEffect(() => {
-    setCompletedCount(data?.todos?.[0]?.todos?.filter(todo => todo.status === "COMPLETED").length ?? 0);
-  }, [data]); 
-
   return (
     <div css={boardStyle}>
       <MyPotIcon css={iconStyle} />
       <div css={boardTextStyle}>
         {data?.title ?? ""}- 안녕하세요, <span css={highlightStyle}>{data?.nickname ?? ""}</span> 님! 오늘 할 일이{" "}
-        <span css={highlightStyle}>{(data?.taskCount ?? 0) - completedCount}</span>개 있어요.
+        <span css={highlightStyle}>{data?.taskCount ?? 0}</span>개 있어요.
       </div>
     </div>
   );
