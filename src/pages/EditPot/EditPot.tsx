e30fbafceba8c8a548742dca58a29436a3fc63e6
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-02-20 19:53:16 +0900

    refactor: onSuccess 동작 훅에서 처리

--- src/pages/EditPot/EditPot.tsx
+++ src/pages/EditPot/EditPot.tsx
@@ -1,12 +1,10 @@
 import { mainContainer } from "./Editpot.style";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import usePatchPot from "apis/hooks/pots/usePatchPot";
 import useGetPotDetail from "apis/hooks/pots/useGetPotDetail";
-import routes from "@constants/routes";
 import PotForm, { PotFormData } from "@pages/CreatePot/components/PotForm";
 
 const EditPot = () => {
-  const navigate = useNavigate();
   const { potId } = useParams();
   const potIdNumber = Number(potId);
   const { mutate: editPot } = usePatchPot();
@@ -16,8 +14,6 @@ const EditPot = () => {
     editPot({
       potId: potIdNumber,
       body: data
-    }, {
-      onSuccess: () => navigate(`${routes.pot.base}/${potId}`)
     })
   }
 
