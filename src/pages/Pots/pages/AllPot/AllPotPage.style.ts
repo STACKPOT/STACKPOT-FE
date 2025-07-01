227c785c3eb7115bf5e75ebbc5ce0a1f3da343d1
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-07-02 01:56:03 +0900

    style: 모든 팟 페이지 스타일 수정

--- src/pages/Pots/pages/AllPot/AllPotPage.style.ts
+++ src/pages/Pots/pages/AllPot/AllPotPage.style.ts
@@ -16,11 +16,13 @@ export const ctaCardWrapper = css`
 `;
 
 export const potCardContainer = css`
+  width: fit-content;
   display: grid;
-  grid-template-columns: repeat(3, auto);
+  grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(3, 1fr);
   gap: 3.2rem 3.7rem;
   justify-content: center;
+  align-self: center;
   margin-top: 3.2rem;
 `;
 
