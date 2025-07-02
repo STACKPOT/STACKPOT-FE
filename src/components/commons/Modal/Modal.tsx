336dd891f9988adba4a1420a8f11a6ebaac4aa05
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-07-02 19:37:20 +0900

    docs: 모달 컴포넌트 주석 작성

--- src/components/commons/Modal/Modal.tsx
+++ src/components/commons/Modal/Modal.tsx
@@ -11,15 +11,56 @@ import { CloseIcon } from "@assets/svgs";
 import Button from "../Button/Button";
 
 interface ModalProps {
+  /** 모달의 제목 */
   title: string;
+
+  /** 모달의 본문 텍스트 */
   message: string;
+
+  /**
+   * _확인 버튼_ 스타일 타입
+   * - `"normal"`: 파란색 버튼
+   * - `"neg"` : 빨간색 버튼
+   *
+   * @default "normal"
+   */
   confirmType?: "normal" | "neg";
+
+  /**
+   * 취소 버튼에 표시될 문구.
+   * 기본 값은 "취소"
+   */
   cancelButton?: string;
+
+  /**
+   * 확인 버튼에 표시될 문구.
+   * 기본값은 "동의합니다"
+   */
   confirmButton?: string;
+
+  /** 확인 버튼 클릭 시 호출되는 콜백함수 */
   onConfirm: () => void;
+
+  /** 취소 버튼 또는 닫기 아이콘 클릭 시 호출되는 콜백함수.*/
   onCancel: () => void;
 }
 
+/**
+ * 확인/취소 두개의 선택지를 가지는 모달 컴포넌트
+ *
+ * @example
+ * ```tsx
+ * <Modal
+ *  title={`팟을\n삭제하시겠습니까?`}
+ *  message="삭제하시면 복구할 수 없습니다. 정말로 삭제할까요?"
+ *  confirmType="neg"
+ *  cancelButton="취소하기"
+ *  confirmButton="삭제"
+ *  onCancel={() => setShowDeleteModal(false)}
+ *  onConfirm={handleDeletePot}
+ * />
+ * ```
+ */
 const Modal: React.FC<ModalProps> = ({
   title,
   message,
