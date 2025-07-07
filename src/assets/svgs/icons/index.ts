20aad675cbb8bd986d4565fc5340cefd760a1bb2
Sohyunnnn <thgus05061@naver.com>
2025-07-07 10:38:19 +0900

    asset: add asset

--- src/assets/svgs/icons/index.ts
+++ src/assets/svgs/icons/index.ts
@@ -29,3 +29,4 @@ export { default as FillSaveIcon } from "./fillSave.svg?react";
 export { default as AddIcon } from "./add.svg?react";
 export { default as SearchBlueIcon } from "./searchBlue.svg?react";
 export { default as BellIcon } from "./bell.svg?react";
+export { default as BellFilledIcon } from "./bellFilled.svg?react";
