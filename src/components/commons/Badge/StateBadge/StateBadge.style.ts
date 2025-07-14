c740ae0da5d5f6f4c48ee1072fc0be917fd49581
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-07-14 16:36:18 +0900

    refactor: 코드 수정

--- src/components/commons/Badge/StateBadge/StateBadge.style.ts
+++ src/components/commons/Badge/StateBadge/StateBadge.style.ts
@@ -3,22 +3,23 @@ import theme from "@styles/theme";
 import { PotStatus } from "types/potStatus";
 import { AnotherTaskStatus } from "types/taskStatus";
 
-export const taskBadgeStyle = (content: AnotherTaskStatus, clickable: boolean = false) => css`
+export const taskBadgeStyle = (
+  content: AnotherTaskStatus,
+  clickable = false
+) => css`
   display: flex;
   padding: 1.2rem 2rem;
   border-radius: 8px;
   background-color: ${content === "진행 전"
     ? theme.color.accent.redBg
     : content === "진행 중"
-      ? theme.color.accent.yellowBg
-      : theme.color.accent.greenBg
-  };
+    ? theme.color.accent.yellowBg
+    : theme.color.accent.greenBg};
   color: ${content === "진행 전"
     ? theme.color.accent.redFg
     : content === "진행 중"
-      ? theme.color.accent.pinkFg
-      : theme.color.status.positive
-  };
+    ? theme.color.accent.pinkFg
+    : theme.color.status.positive};
   ${theme.font.captionBold1};
   box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.16);
   white-space: nowrap;
@@ -33,15 +34,13 @@ export const potBadgeStyle = (type: PotStatus) => css`
   background-color: ${type === "ONGOING"
     ? theme.color.accent.yellowBg
     : type === "COMPLETED"
-      ? theme.color.accent.pinkBg
-      : theme.color.accent.blueBg
-  };
+    ? theme.color.accent.pinkBg
+    : theme.color.accent.blueBg};
   color: ${type === "ONGOING"
     ? theme.color.accent.yellowFg
     : type === "COMPLETED"
-      ? theme.color.accent.pinkFg
-      : theme.color.point.darkblue
-  };
+    ? theme.color.accent.pinkFg
+    : theme.color.point.darkblue};
   ${theme.font.caption1};
   white-space: nowrap;
 `;
