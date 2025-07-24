f11ec348e8015ca9fd47d1da745025dfa65fc2a1
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-07-24 22:54:51 +0900

    feat: 팟 끓이기 로딩 및 navigate 수정

--- src/pages/CreateFinishedPot/CreateFinishedPot.tsx
+++ src/pages/CreateFinishedPot/CreateFinishedPot.tsx
@@ -1,28 +1,38 @@
 import routes from "@constants/routes";
 import { FinishedPotForm } from "@pages/EditFinishedPot/components";
+import { Loading } from "@pages/MyPotDetail/components";
 import usePatchPotComplete from "apis/hooks/pots/usePatchPotComplete";
 import { PatchPotCompleteBody } from "apis/types/pot";
 import { useNavigate, useParams } from "react-router-dom";
 
 const CreateFinishedPot = () => {
-    const navigate = useNavigate();
-    const { potId } = useParams();
-    const potIdNumber = Number(potId);
-    const { mutate } = usePatchPotComplete();
+  const navigate = useNavigate();
+  const { potId } = useParams();
+  const potIdNumber = Number(potId);
+  const { mutate, isPending } = usePatchPotComplete();
 
-    const handleUpload = (data: PatchPotCompleteBody) => {
-        mutate({
-            potId: potIdNumber,
-            body: data
-        }, {
-            onSuccess: () => {
-                navigate(`${routes.pot.madeByMe}`)
-            }
-        })
-    };
+  const handleUpload = (data: PatchPotCompleteBody) => {
+    mutate(
+      {
+        potId: potIdNumber,
+        body: data,
+      },
+      {
+        onSuccess: () => {
+          navigate(`${routes.finishedPot}/${potIdNumber}`);
+        },
+      }
+    );
+  };
 
-    return (
-        <FinishedPotForm potId={potIdNumber} type="create" onCompleted={handleUpload} />
-    )
-}
+  if (isPending) return <Loading />;
+
+  return (
+    <FinishedPotForm
+      potId={potIdNumber}
+      type="create"
+      onCompleted={handleUpload}
+    />
+  );
+};
 export default CreateFinishedPot;
