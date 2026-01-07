befa1163380a4711e009d682c796655cbe8768c3
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2026-01-07 17:08:25 +0900

    refactor: 줄바꿈 수정

--- src/components/commons/Modal/LoginModal/LoginModal.tsx
+++ src/components/commons/Modal/LoginModal/LoginModal.tsx
@@ -20,11 +20,11 @@ interface LoginModalProps {
 }
 
 const LoginModal: React.FC<LoginModalProps> = ({ onCancel }) => {
-  const googleLoginLink = `https://accounts.google.com/o/oauth2/v2/auth?
-client_id=${import.meta.env.VITE_REST_GOOGLE_API_KEY}
-&redirect_uri=${import.meta.env.VITE_GOOGLE_REDIRECT_URI}
-&response_type=code
-&scope=email`;
+  const googleLoginLink = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
+    import.meta.env.VITE_REST_GOOGLE_API_KEY
+  }&redirect_uri=${
+    import.meta.env.VITE_GOOGLE_REDIRECT_URI
+  }&response_type=code&scope=email`;
 
   const kakaoLoginLink = `https://kauth.kakao.com/oauth/authorize?client_id=${
     import.meta.env.VITE_REST_API_KEY
